3628999da628d4a8c3a98d633e811f4a159a7a55
Stijn Bosch <stijndebosch@gmail.com>
2025-06-25 18:12:39 +0200

    chopped it up

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 jupyter-book build --builder html --path-output temp_build .
 
 # Importeer de inhoud van temp_build direct in de root van gh-pages branch
-ghp-import -n -p -f temp_build/_build
+ghp-import -n -p -f temp_build/_build/html
 
 # Optioneel: verwijder de tijdelijke folder
 rm -rf temp_build
